99f77c2e43f542eabcaaa88a917c53acfbdbc3e7
jennifershan <79737766+jennifershan@users.noreply.github.com>
2023-07-13 23:56:05 -0400

    Update send_to_receive.m

--- send_to_receive.m
+++ send_to_receive.m
@@ -14,16 +14,6 @@ function receiver_result = send_to_receive(sender_bases, receiver_bases, key_sta
             else
                 qubit = hadamard(qubit);
             end
-        %{
-        else
-            if qubit == 0
-                qubit = pauli_z([1; 0]);
-            elseif qubit == 1
-                qubit = pauli_z([0; 1]);
-            else
-                qubit = pauli_z(qubit);
-            end
-        %}
         end
         if receiver_bases(i) > 0
             if qubit == 0
@@ -33,16 +23,6 @@ function receiver_result = send_to_receive(sender_bases, receiver_bases, key_sta
             else
                 qubit = hadamard(qubit);
             end
-        %{
-        else
-            if qubit == 0
-                qubit = pauli_z([1; 0]);
-            elseif qubit == 1
-                qubit = pauli_z([0; 1]);
-            else
-                qubit = pauli_z(qubit);
-            end
-        %}
         end
         receiver_result = [receiver_result measure(qubit)];
     end    
